71111243a68bbb3daa2858868c3fe44a1e01f942
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-04-28 22:21:13 -0700

    feat: deploy label studio

---
+++ scripts/deploy_label_studio_pass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+NODE=$HOME/.nvm/versions/node/v22.9.0/bin/node
+BW=$HOME/.nvm/versions/node/v22.9.0/lib/node_modules/@bitwarden/cli/build/bw.js
+BW_SESSION=`cat .bw_session`
+LABEL_ADMIN_PASSWD=`$NODE $BW --session $BW_SESSION get password "E4E Label Studio Admin Password"`
+PG_PASSWD=`$NODE $BW --session $BW_SESSION get password "fabricant label_studio postgres"`
+echo "LABEL_STUDIO_PASSWORD=\"$LABEL_ADMIN_PASSWD\"" > $HOME/fabricant/.secrets/label_studio_admin_password_pg.env
+echo "POSTGRES_USER=\"$PG_PASSWD\"" >> $HOME/fabricant/.secrets/label_studio_admin_password_pg.env
