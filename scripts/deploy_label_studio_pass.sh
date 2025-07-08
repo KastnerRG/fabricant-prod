3c451a49da5815b15f6989d7a869dc1ed564fda2
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-07-08 11:32:21 -0700

    chore: rename fabricant to fabricant-prod

--- scripts/deploy_label_studio_pass.sh
+++ scripts/deploy_label_studio_pass.sh
@@ -4,5 +4,5 @@ BW=$HOME/.nvm/versions/node/v24.3.0/lib/node_modules/@bitwarden/cli/build/bw.js
 BW_SESSION=`cat .bw_session`
 LABEL_ADMIN_PASSWD=`$NODE $BW --session $BW_SESSION get password "E4E Label Studio Admin Password"`
 PG_PASSWD=`$NODE $BW --session $BW_SESSION get password "fabricant label_studio postgres"`
-echo "LABEL_STUDIO_PASSWORD=\"$LABEL_ADMIN_PASSWD\"" > $HOME/fabricant/.secrets/label_studio_admin_password_pg.env
-echo "POSTGRES_USER=\"$PG_PASSWD\"" >> $HOME/fabricant/.secrets/label_studio_admin_password_pg.env
+echo "LABEL_STUDIO_PASSWORD=\"$LABEL_ADMIN_PASSWD\"" > $HOME/fabricant-prod/.secrets/label_studio_admin_password_pg.env
+echo "POSTGRES_USER=\"$PG_PASSWD\"" >> $HOME/fabricant-prod/.secrets/label_studio_admin_password_pg.env
