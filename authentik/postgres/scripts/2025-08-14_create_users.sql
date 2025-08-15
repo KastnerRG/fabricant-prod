beefed18452700c3ebe7aaadf89d099ad13d6120
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-08-15 16:34:56 -0700

    Update 2025-08-14_create_users.sql

--- authentik/postgres/scripts/2025-08-14_create_users.sql
+++ authentik/postgres/scripts/2025-08-14_create_users.sql
@@ -4,5 +4,5 @@ CREATE ROLE authentik WITH
     LOGIN
     ENCRYPTED PASSWORD 'SCRAM-SHA-256$4096:hTZDgCEdj6ygbNEgktVjcw==$ZHPpYXMxvX5RvsUUlu+RVWaSzlHl5gsxE0h4yBUsHL4=:KjW1TAQD0awpAF2XmwOWPBW8/Ea2EyDuFcUFdSsk2ME=';
 GRANT ALL PRIVILEGES ON DATABASE authentik_db TO authentik;
-\c authentik postgres
+\c authentik_db postgres
 GRANT ALL ON SCHEMA public TO authentik;
