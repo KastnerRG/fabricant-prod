b543aed2e72b1e7b82d64e8331e282ed7ff927ca
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-11-11 10:26:46 -0800

    fix: always set the postgres password

--- scripts/deploy_authentik_pass.sh
+++ scripts/deploy_authentik_pass.sh
@@ -6,3 +6,4 @@ AUTHENTIK_SECRET_KEY=`$NODE $BW --session $BW_SESSION get password "authentik se
 AUTHENTIK_POSTGRESQL__PASSWORD=`$NODE $BW --session $BW_SESSION get password "9157437f-bdb0-49c5-8538-ac0638b8d145"`
 echo "AUTHENTIK_SECRET_KEY=\"$AUTHENTIK_SECRET_KEY\"" > $HOME/fabricant-prod/.secrets/authentik_admin_password.env
 echo "AUTHENTIK_POSTGRESQL__PASSWORD=\"$AUTHENTIK_POSTGRESQL__PASSWORD\"" >> $HOME/fabricant-prod/.secrets/authentik_admin_password.env
+echo "POSTGRES_PASSWORD=\"$AUTHENTIK_POSTGRESQL__PASSWORD\"" >> $HOME/fabricant-prod/.secrets/authentik_admin_password.env
