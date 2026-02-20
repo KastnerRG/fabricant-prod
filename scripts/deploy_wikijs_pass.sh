46dfad842228f42e41e9692430e33288d46d6779
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2026-02-19 21:21:57 -0800

    fix: remove wikijs

--- scripts/deploy_wikijs_pass.sh
+++
@@ -1,9 +0,0 @@
-# wikijs - database
-
-#!/bin/bash
-NODE=$HOME/.nvm/versions/node/v24.3.0/bin/node
-BW=$HOME/.nvm/versions/node/v24.3.0/lib/node_modules/@bitwarden/cli/build/bw.js
-BW_SESSION=`cat .bw_session`
-WIKIJS_POSTGRESQL__PASSWORD=`$NODE $BW --session $BW_SESSION get password "wikijs - database"`
-echo "DB_PASS=\"$WIKIJS_POSTGRESQL__PASSWORD\"" >> $HOME/fabricant-prod/.secrets/wikijs_db_password.env
-echo "POSTGRES_PASSWORD=\"$WIKIJS_POSTGRESQL__PASSWORD\"" >> $HOME/fabricant-prod/.secrets/wikijs_db_password.env
